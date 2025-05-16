d0d4719ea12e1d2aedd549417b30f00a081e6f12
Mani Koppisetti <mani.koppisetti@global.ntt>
2025-05-16 21:32:52 +0530

    hello world started

---
+++ 02-conversation.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+echo "Trump: Hey Musk, How are you?"
+echo "Musk:: Hello trump. I am fine. How are you doing"
+echo "Trump: I am fine too.What's up?"
+echo "Musk::Nothing,just going to Mars now,will you come"
+echo "Trump:: Sorry, you carry on! I will come once you back"

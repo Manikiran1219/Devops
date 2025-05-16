ba362dc7e63a75193a65231afa143c30c9c5be09
Mani Koppisetti <mani.koppisetti@global.ntt>
2025-05-16 22:11:09 +0530

    hello world started

--- 03-Variables.sh
+++ 03-Variables.sh
@@ -3,8 +3,8 @@
 PERSON1=Trump
 PERSON2=Musk
 
-echo "Person1: Hey Person2, How are you?"
-echo "Person2:: Hello Person1. I am fine. How are you doing"
-echo "Person1: I am fine too.What's up?"
-echo "Person2::Nothing,just going to Mars now,will you come"
-echo "Person1:: Sorry, you carry on! I will come once you back"
+echo "$Person1: Hey $Person2, How are you?"
+echo "$Person2:: Hello $Person1. I am fine. How are you doing"
+echo "$Person1: I am fine too.What's up?"
+echo "$Person2::Nothing,just going to Mars now,will you come"
+echo "$Person1:: Sorry, you carry on! I will come once you back"
